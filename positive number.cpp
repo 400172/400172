a25a878bc9c8672cd3224c45e4e778788fec464d
400172 <95227629+400172@users.noreply.github.com>
2021-12-01 13:06:35 +0530

    Add files via upload

---
+++ positive number.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int m;
+	scanf("%d",&m);
+	if(m%2==0)
+    {
+	printf("%d is even number", m);
+    }
+    if(m%2!=0)
+    {
+	printf("%d is odd number",m);
+    }
+}
