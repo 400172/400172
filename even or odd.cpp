23c0e84fb1859e200f938a632c322a9462d02470
400172 <95227629+400172@users.noreply.github.com>
2021-12-01 13:07:06 +0530

    Add files via upload

---
+++ even or odd.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int x, y;
+	printf("enter the values for x and y");
+	scanf("%d%d",&x,&y);
+	if (x == y)
+	printf("x and y are equal");
+	else
+	printf("x and y are not equal");
+}
+	
+	
